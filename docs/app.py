0c7e3832ad01cc5204188d83460024f822f42b74
Kevin Wirtz <kevin-w@hotmail.fr>
2024-01-11 16:38:38 +0100

    Update app.py

--- docs/app.py
+++ docs/app.py
@@ -20,8 +20,14 @@ def teachings():
     return render_template('teachings.html')
 
 @app.route('/blogposts')
-def blogpost():
-    return render_template('blogposts.html')
+def blogposts():
+    return render_template('blogposts.html', posts=pages)
+
+@app.route('/blogposts/<int:post_id>')
+def blogpost(post_id):
+    post = next((p for p in pages if p.meta.get('id') == post_id), None)
+    if post:
+        return render_template('blogpost.html', post=post)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
